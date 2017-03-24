6869c93509f47c8e2d9568a9d672a501bd348708
Giuliano <morgan@anouk.oats.inaf.it>
2017-03-24 18:05:30 +0100

    First commit of the project

---
+++ uno.py
@@ -0,0 +1,36 @@
+#!/user/bin/python
+
+# This is just a test for github
+
+"""
+Created on Friday 24 Mar 2017
+by giuliano.taffoni@inaf.it
+"""
+
+
+n = input("some integer value: ") # input from terminal
+
+print n
+
+# variable types are dynamically assigned
+
+n = "uno"
+
+print(n)
+
+'''
+operands in python
+
+    a + b
+    a - b
+    a * b
+    a / b
+    a**b
+    a % b (modulo)
+    or, and, not (logical operators)
+    is, is not  (identity operators)
+    is, in, not in (membership operators)
+
+'''
+
+
